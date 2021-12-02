5a50a6256fe39405274fc7fa76476ad6ff1e5694
jval1972 <jvalavanis@gmail.com>
2021-12-02 21:51:09 +0200

    Added palette creation code

--- MAD2WAD.dpr
+++ MAD2WAD.dpr
@@ -5,7 +5,8 @@ uses
   main in 'main.pas' {Form1},
   mw_madreader in 'mw_madreader.pas',
   mw_types in 'mw_types.pas',
-  mw_wadwriter in 'mw_wadwriter.pas';
+  mw_wadwriter in 'mw_wadwriter.pas',
+  mw_palette in 'mw_palette.pas';
 
 {$R *.res}
 
